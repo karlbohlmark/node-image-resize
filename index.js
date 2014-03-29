1298eb18c8bb9955167658951296ec30cf912554
Karl Böhlmark <karl.bohlmark@gmail.com>
2014-03-29 14:32:08 +0100

    ok, file names with dots should be handled

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ module.exports = resize;
 function resize (file, options, cb) {
     var width = options.width;
     var height = options.height;
-    var outfile = options.outfile ||
-                  file.replace('.', '_' + height + 'x' + width + '.');
+    var outfile = options.outfile
+    if (!outfile) {
+        var size = height + 'x' + width;
+        var lastDot = file.lastIndexOf(".");
+        var ext = file.substring(lastDot, file.length);
+        outfile = file.substring(0, lastDot) + "_" + size + ext;
+    }
 
     sharp(file)
         .resize(width, height)
