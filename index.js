6ee1e2e07eef9a63399c4732c22986e8cd2028bb
Karl Böhlmark <karl.bohlmark@gmail.com>
2014-04-13 15:35:15 +0200

    add tests

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function resize (file, options, cb) {
     var outfile = options.outfile;
     if (!outfile) {
         // Default naming: 'img.png' resized to 300x200 -> 'img_300x200.png'
-        var size = height + 'x' + width;
+        var size = width + 'x' + height;
         var lastDot = file.lastIndexOf(".");
         var ext = file.substring(lastDot, file.length);
         outfile = file.substring(0, lastDot) + "_" + size + ext;
@@ -18,6 +18,7 @@ function resize (file, options, cb) {
     if (options.outdir) {
       outfile = path.join(options.outdir, outfile);
     }
+
     sharp(file)
         .resize(width, height)
         .write(outfile, cb);
---
+++ test/index.js
@@ -0,0 +1,81 @@
+var test = require('tape');
+var SandboxedModule = require('sandboxed-module');
+
+
+// Mocking setup
+var sharp;
+var resize = SandboxedModule.require('../', {
+  requires: {
+    'sharp': function (file) { return sharp = new MockSharp(file); },
+  }
+});
+
+
+// Tests
+test('passes params to sharp', function (t) {
+    t.plan(3);
+
+    var target = {
+        input: "file.png",
+        options: {
+            width: 100,
+            height: 200    
+        }
+    }
+    resize(target.input, target.options, function () {
+        console.log('done')
+    })
+
+    t.equal(sharp.input, target.input);
+    t.equal(sharp.width, target.options.width);
+    t.equal(sharp.height, target.options.height);
+});
+
+test('generates outfile name from dimensions', function (t) {
+    t.plan(1);
+
+    var target = {
+        input: "file.png",
+        options: {
+            width: 100,
+            height: 200    
+        }
+    }
+    resize(target.input, target.options, function () {
+        console.log('done')
+    })
+
+    t.equal(sharp.outfile, "file_100x200.png");
+});
+
+test('joins outdir to outfile ', function (t) {
+    t.plan(1);
+
+    var target = {
+        input: "file.png",
+        options: {
+            outdir: "resized",
+            width: 100,
+            height: 200    
+        }
+    }
+    resize(target.input, target.options, function () {
+        console.log('done')
+    })
+
+    t.equal(sharp.outfile, "resized/file_100x200.png");
+});
+
+// Mocking utilities
+function MockSharp(input) {
+    this.input = input
+}
+MockSharp.prototype.resize = function (w, h) {
+    this.width = w;
+    this.height = h;
+    return this;
+}
+MockSharp.prototype.write = function (outfile) {
+    this.outfile = outfile;
+    return this;
+}
